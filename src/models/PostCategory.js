cc06595f94e4fb0cc83dbcc0d8f523a9ab62965d
Ariane Padilha <ariane.padilha@outlook.com.br>
2022-10-26 00:54:21 -0300

    ✨ Req 11 - created model PostCategory

---
+++ src/models/PostCategory.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    categoryId: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    }
+  }, {
+    tableName: 'post_categories',
+    timestamps: false,
+    underscored: true,
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'blog_posts',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
