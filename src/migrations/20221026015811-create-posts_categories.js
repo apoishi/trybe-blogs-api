a8ee41e5ab8a9bfaa16e0dfdb48082b99a766bd6
Ariane Padilha <ariane.padilha@outlook.com.br>
2022-10-25 23:42:11 -0300

    ✨ Req 01 - created migrations

---
+++ src/migrations/20221026015811-create-posts_categories.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('posts_categories', {
+      post_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'blog_posts',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      category_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('posts_categories');
+  },
+};
